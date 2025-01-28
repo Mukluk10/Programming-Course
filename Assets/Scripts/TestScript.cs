2e010899f156a09000dce5962ca923914fd20c3f
Mukluk10 <pyry.urala@gmail.com>
2025-01-28 10:31:14 +0200

    Wrote a script

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+namespace AG3961
+{
+    public class TestScript : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private float time;
+        [SerializeField] private bool goinUp;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            goinUp = false;
+            StartCoroutine(SwitchDirection());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (goinUp)
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+
+            if(goinUp == false)
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+            
+        }
+
+        IEnumerator SwitchDirection()
+        {
+            yield return new WaitForSeconds(time);
+            if (goinUp)
+            {
+                goinUp = false;
+            }
+            else
+            {
+                goinUp = true;
+            }
+            StartCoroutine(SwitchDirection());
+        }
+    }
+}
